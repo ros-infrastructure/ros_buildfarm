84754fe991dd1f6e42bafc276436c6dbdaa8eabe
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2014-11-23 21:09:56 +0100

    Use a workaround for get_repository_url when git is not installed.

--- ros_buildfarm/git.py
+++ ros_buildfarm/git.py
@@ -1,3 +1,4 @@
+import configparser
 import os
 import subprocess
 
@@ -9,8 +10,8 @@ def get_ros_buildfarm_url():
 
 
 def get_repository_url(path=None):
-    url = subprocess.check_output(['git', 'config', 'remote.origin.url'])
-    url = url.decode().rstrip()
+    url = _try_get_repository_url(path)
+    url = url.rstrip()
     prefix_mapping = {
         'git@github.com:': 'https://github.com/',
     }
@@ -18,3 +19,61 @@ def get_repository_url(path=None):
         if url.startswith(k):
             url = v + url[len(k):]
     return url
+
+
+def _try_get_repository_url(path):
+    # Try using native git
+    url = _get_repository_url_with_native_git(path)
+    if url is not None:
+        return url
+    # Try using emulation
+    print('Falling back to directly reading the git '
+          'config file to obtain the repository url.')
+    url = _get_repository_url_with_emulation(path)
+    # Check success
+    assert url is not None, "get_repository_url() failed for path %s" % path
+    return url
+
+
+def _get_repository_url_with_native_git(path):
+    try:
+        return subprocess.check_output(['git', 'config', 'remote.origin.url'], universal_newlines=True)
+    except FileNotFoundError as error:
+        print("Native git executable is not available (%s)." % error)
+        return None
+
+
+def _get_repository_url_with_emulation(path):
+    """
+    Alternative for _get_repository_url_with_native_git when git is not available (e.g. inside Docker).
+    """
+    git_root = _find_git_root(path)
+    repo_url = _read_git_config(git_root, 'remote "origin"', "url") if git_root is not None else None
+    return repo_url
+
+
+def _find_git_root(path=None):
+    path = os.getcwd() if path is None else path
+    path = os.path.abspath(path)
+    for parent_dir in _walkup(path):
+        git_dir = os.path.join(parent_dir, '.git')
+        if os.path.exists(git_dir):
+            return git_dir
+    return None
+
+
+def _walkup(path):
+    at_top = False
+    while not at_top:
+        yield path
+        parent_path = os.path.dirname(path)
+        if parent_path == path:
+            at_top = True
+        else:
+            path = parent_path
+
+
+def _read_git_config(git_root, section, key):
+    config = configparser.ConfigParser()
+    config.read(os.path.join(git_root, 'config'))
+    return config[section][key]
