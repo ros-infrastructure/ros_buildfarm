0ae556e9dfa47f700f202575da53bf5906e3b9d1
Dirk Thomas <dthomas@osrfoundation.org>
2015-01-06 10:11:57 -0800

    fix generator script for individual devel jobs

--- scripts/devel/generate_devel_job.py
+++ scripts/devel/generate_devel_job.py
@@ -5,10 +5,10 @@ import sys
 
 from ros_buildfarm.argument import add_argument_arch
 from ros_buildfarm.argument import add_argument_build_name
+from ros_buildfarm.argument import add_argument_config_url
 from ros_buildfarm.argument import add_argument_os_code_name
 from ros_buildfarm.argument import add_argument_os_name
 from ros_buildfarm.argument import add_argument_repository_name
-from ros_buildfarm.argument import add_argument_rosdistro_index_url
 from ros_buildfarm.argument import add_argument_rosdistro_name
 from ros_buildfarm.devel_job import configure_devel_job
 
@@ -16,7 +16,7 @@ from ros_buildfarm.devel_job import configure_devel_job
 def main(argv=sys.argv[1:]):
     parser = argparse.ArgumentParser(
         description="Generate a 'devel' job on Jenkins")
-    add_argument_rosdistro_index_url(parser)
+    add_argument_config_url(parser)
     add_argument_rosdistro_name(parser)
     add_argument_build_name(parser, 'source')
     add_argument_repository_name(parser)
@@ -26,8 +26,9 @@ def main(argv=sys.argv[1:]):
     args = parser.parse_args(argv)
 
     return configure_devel_job(
-        args.rosdistro_index_url, args.rosdistro_name, args.source_build_name,
-        args.repository_name, args.os_name, args.os_code_name, args.arch)
+        args.config_url, args.rosdistro_name, args.source_build_name,
+        args.repository_name, args.os_name, args.os_code_name, args.arch,
+        False)
 
 
 if __name__ == '__main__':
