544a090b843482a5a4277de7de498cb8eb614859
Dirk Thomas <dthomas@osrfoundation.org>
2015-01-14 17:19:24 -0800

    refactor recursive templates to use single interpreter

--- ros_buildfarm/templates/__init__.py
+++ ros_buildfarm/templates/__init__.py
@@ -13,12 +13,13 @@ from xml.sax.saxutils import escape
 
 template_basepath = os.path.abspath(os.path.dirname(__file__))
 
-
+interpreter = None
 template_hook = None
 
 
-def expand_template(
-        template_name, data, options=None, add_context_variables=True):
+def expand_template(template_name, data, options=None):
+    global template_basepath
+    global interpreter
     global template_hook
 
     output = StringIO()
@@ -27,20 +28,20 @@ def expand_template(
         template_path = os.path.join(template_basepath, template_name)
         # create copy before manipulating
         data = dict(data)
-        if add_context_variables:
-            data['template_name'] = template_name
-            now = time.localtime()
-            data['now_str'] = time.strftime(
-                '%Y-%m-%d %H:%M:%S %z', now)
-            data['today_str'] = time.strftime(
-                '%Y-%m-%d (%z)', now)
-            tz_name = time.strftime('%Z', now)
-            tz_offset = time.strftime('%z', now)
-            data['timezone'] = '%s%s%s' % \
-                (tz_name, '+' if tz_offset[0] == '-' else '-', tz_offset[1:3])
-        data['ESCAPE'] = _escape_value
-        data['TEMPLATE'] = _expand_template
-        data['SNIPPET'] = _expand_snippet
+        # add some generic information to context
+        data['template_name'] = template_name
+        now = time.localtime()
+        data['now_str'] = time.strftime(
+            '%Y-%m-%d %H:%M:%S %z', now)
+        data['today_str'] = time.strftime(
+            '%Y-%m-%d (%z)', now)
+        tz_name = time.strftime('%Z', now)
+        tz_offset = time.strftime('%z', now)
+        data['timezone'] = '%s%s%s' % \
+            (tz_name, '+' if tz_offset[0] == '-' else '-', tz_offset[1:3])
+
+        _add_helper_functions(data)
+
         interpreter.file(open(template_path, 'r'), locals=data)
         value = output.getvalue()
         if template_hook:
@@ -52,6 +53,13 @@ def expand_template(
         raise
     finally:
         interpreter.shutdown()
+        interpreter = None
+
+
+def _add_helper_functions(data):
+    data['ESCAPE'] = _escape_value
+    data['SNIPPET'] = _expand_snippet
+    data['TEMPLATE'] = _expand_template
 
 
 def _escape_value(value):
@@ -66,18 +74,22 @@ def _escape_value(value):
 
 def _expand_snippet(snippet_name, **kwargs):
     template_name = 'snippet/%s.xml.em' % snippet_name
-    return _expand_template(template_name, **kwargs)
+    _expand_template(template_name, **kwargs)
 
 
 def _expand_template(template_name, **kwargs):
-    try:
-        value = expand_template(
-            template_name, kwargs, add_context_variables=False)
-        return value
-    except Error as e:
-        print("%s in template '%s': %s" %
-              (e.__class__.__name__, template_name, str(e)), file=sys.stderr)
-        sys.exit(1)
+    global template_basepath
+    global interpreter
+    template_path = os.path.join(template_basepath, template_name)
+    _add_helper_functions(kwargs)
+    with open(template_path, 'r') as h:
+        try:
+            interpreter.include(h, kwargs)
+        except Exception as e:
+            print(
+                "%s in template '%s': %s" %
+                (e.__class__.__name__, template_name, str(e)), file=sys.stderr)
+            sys.exit(1)
 
 
 def create_dockerfile(template_name, data, dockerfile_dir):
