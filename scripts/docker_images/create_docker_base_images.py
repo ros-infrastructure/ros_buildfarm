fd0563a73482686d1936ae31ea2142e6ef6bac19
Tully Foote <tfoote@osrfoundation.org>
2015-02-10 00:56:26 -0800

    simple docker image generator proof of concept.

---
+++ scripts/docker_images/create_docker_base_images.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
+
+from apt import Cache
+from catkin_pkg.packages import find_packages
+from ros_buildfarm.argument import \
+    add_argument_distribution_repository_key_files
+from ros_buildfarm.argument import add_argument_distribution_repository_urls
+from ros_buildfarm.argument import add_argument_dockerfile_dir
+from ros_buildfarm.common import get_binary_package_versions
+from ros_buildfarm.common import get_debian_package_name
+from ros_buildfarm.common import get_distribution_repository_keys
+from ros_buildfarm.common import get_user_id
+from ros_buildfarm.templates import create_dockerfile
+
+
+def main(argv=sys.argv[1:]):
+    parser = argparse.ArgumentParser(
+        description="Generate the 'Dockerfile's for the base docker images")
+    parser.add_argument(
+        '--rosdistro-name',
+        required=True,
+        help='The name of the ROS distro to identify the setup file to be '
+             'sourced')
+    parser.add_argument(
+        '--packages',
+        nargs='+',
+        help='What (meta)packages to include in the image.')
+    parser.add_argument(
+        '--os-name',
+        required=True,
+        help="The OS name (e.g. 'ubuntu')")
+    parser.add_argument(
+        '--os-code-name',
+        required=True,
+        help="The OS code name (e.g. 'trusty')")
+    parser.add_argument(
+        '--arch',
+        required=True,
+        help="The architecture (e.g. 'amd64')")
+    add_argument_distribution_repository_urls(parser)
+    add_argument_distribution_repository_key_files(parser)
+    add_argument_dockerfile_dir(parser)
+    args = parser.parse_args(argv)
+
+    pkg_names = args.packages
+    print("Found the following packages:")
+    for pkg_name in sorted(pkg_names):
+        print('  -', pkg_name)
+
+
+    # generate Dockerfile
+    data = {
+        'os_name': args.os_name,
+        'os_code_name': args.os_code_name,
+        'arch': args.arch,
+
+        'distribution_repository_urls': args.distribution_repository_urls,
+        'distribution_repository_keys': get_distribution_repository_keys(
+            args.distribution_repository_urls,
+            args.distribution_repository_key_files),
+
+        'packages': pkg_names,
+        'rosdistro': args.rosdistro_name,
+
+
+    }
+    create_dockerfile(
+        'docker_images/create_base_image.Dockerfile.em', data, args.dockerfile_dir)
+
+
+
+
+def get_dependencies(pkgs, label, get_dependencies_callback):
+    pkg_names = [pkg.name for pkg in pkgs]
+    depend_names = set([])
+    for pkg in pkgs:
+        depend_names.update(
+            [d.name for d in get_dependencies_callback(pkg)
+             if d.name not in pkg_names])
+    print('Identified the following %s dependencies ' % label +
+          '(ignoring packages available from source):')
+    for depend_name in sorted(depend_names):
+        print('  -', depend_name)
+    return depend_names
+
+
+def _get_build_dependencies(pkg):
+    return pkg.build_depends + pkg.buildtool_depends
+
+
+def _get_run_and_test_dependencies(pkg):
+    return pkg.build_export_depends + pkg.buildtool_export_depends + \
+        pkg.exec_depends + pkg.test_depends
+
+
+def initialize_resolver(rosdistro_name, os_name, os_code_name):
+    # resolve rosdep keys into binary package names
+    ctx = create_default_installer_context()
+    try:
+        installer_key = ctx.get_default_os_installer_key(os_name)
+    except KeyError:
+        raise RuntimeError(
+            "Could not determine the rosdep installer for '%s'" % os_name)
+    installer = ctx.get_installer(installer_key)
+    view = get_catkin_view(rosdistro_name, os_name, os_code_name, update=False)
+    return {
+        'os_name': os_name,
+        'os_code_name': os_code_name,
+        'installer': installer,
+        'view': view,
+    }
+
+
+def resolve_names(rosdep_keys, os_name, os_code_name, view, installer):
+    debian_pkg_names = set([])
+    for rosdep_key in sorted(rosdep_keys):
+        try:
+            resolved_names = resolve_for_os(
+                rosdep_key, view, installer, os_name, os_code_name)
+        except KeyError:
+            raise RuntimeError(
+                "Could not resolve the rosdep key '%s'" % rosdep_key)
+        debian_pkg_names.update(resolved_names)
+    print('Resolved the dependencies to the following binary packages:')
+    for debian_pkg_name in sorted(debian_pkg_names):
+        print('  -', debian_pkg_name)
+    return debian_pkg_names
+
+
+def order_dependencies(binary_package_names):
+    return sorted(binary_package_names)
+
+
+if __name__ == '__main__':
+    main()
