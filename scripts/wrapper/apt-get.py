e81a952f69f4bbe3f4c2837e3d29d1a3a10ca907
Dirk Thomas <dthomas@osrfoundation.org>
2015-02-19 16:43:10 -0800

    simplify apt-get wrapper script

--- scripts/wrapper/apt-get.py
+++ scripts/wrapper/apt-get.py
@@ -22,16 +22,14 @@ def main(argv=sys.argv[1:]):
 
 
 def call_apt_get_update_and_install(
-        install_argv, known_error_strings, max_tries_per_command):
-    update_tries = 0
-    install_tries = 0
+        install_argv, known_error_strings, max_tries):
+    tries = 0
     command = 'update'
-    while update_tries < max_tries_per_command and \
-            install_tries < max_tries_per_command:
+    while tries < max_tries:
         if command == 'update':
-            rc, _, update_tries = call_apt_get_repeatedly(
-                [command], known_error_strings,
-                max_tries_per_command - update_tries, offset=update_tries)
+            rc, _, tries = call_apt_get_repeatedly(
+                [command], known_error_strings, max_tries - tries,
+                offset=tries)
             if rc != 0:
                 # abort if update was unsuccessful even after retries
                 break
@@ -41,22 +39,21 @@ def call_apt_get_update_and_install(
         if command == 'install':
             known_error_strings_redo_update = [
                 'Size mismatch', 'maybe run apt-get update']
-            rc, known_error_conditions, install_tries = \
-                call_apt_get_repeatedly(
+            rc, known_error_conditions = \
+                call_apt_get(
                     [command] + install_argv,
-                    known_error_strings + known_error_strings_redo_update, 1,
-                    offset=install_tries)
-            if rc == 0 or not known_error_conditions:
+                    known_error_strings + known_error_strings_redo_update)
+            if rc == 0 or not (
+                    set(known_error_conditions) &
+                    set(known_error_strings_redo_update)):
                 # abort if install was successful
                 # or failed with an unknown errror condition
                 break
-            if set(known_error_conditions) & \
-                    set(known_error_strings_redo_update):
-                # restart with update command
-                print(("'apt-get %s' failed and likely requires 'apt-get " +
-                       "update' to run again") % command)
-                command = 'update'
-            # else try install again
+
+            # restart with update command
+            print("'apt-get install' failed and likely requires 'apt-get " +
+                  "update' to run again")
+            command = 'update'
 
     return rc
 
@@ -73,7 +70,7 @@ def call_apt_get_repeatedly(argv, known_error_strings, max_tries, offset=0):
         if rc == 0 or not known_error_conditions:
             break
         print('')
-        print('Invocation failed due to: %s' %
+        print('Invocation failed due to the following known error conditions: '
               ', '.join(known_error_conditions))
         print('')
         # retry in case of failure with known error condition
@@ -84,6 +81,7 @@ def call_apt_get(argv, known_error_strings):
     known_error_conditions = []
 
     cmd = ['apt-get'] + argv
+    print("Invoking '%s'" % ' '.join(cmd))
     with subprocess.Popen(
             cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as proc:
         while True:
