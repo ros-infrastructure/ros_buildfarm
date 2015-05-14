e9ec0a647b31ae9be4201f4f8745a517d62fc600
William Woodall <william@osrfoundation.org>
2015-05-14 16:43:21 -0700

    added --underlay-packages to the prerelease overlay script adding --json argument for use in prerelease website

--- scripts/prerelease/generate_prerelease_overlay_script.py
+++ scripts/prerelease/generate_prerelease_overlay_script.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import argparse
 from em import BANGPATH_OPT
+import json
 import sys
 
 from catkin_pkg.packages import find_packages
@@ -34,6 +35,16 @@ def main(argv=sys.argv[1:]):
     add_argument_os_code_name(parser)
     add_argument_arch(parser)
     add_overlay_arguments(parser)
+    parser.add_argument(
+        '--underlay-packages', nargs='+',
+        help='Names of packages on which the overlay builds '
+             '(by default package names come from packages found in '
+             "'catkin_workspace/src')"
+    )
+    parser.add_argument(
+        '--json', action='store_true',
+        help='Output overlay information as JSON instead of a shell script'
+    )
 
     args = parser.parse_args(argv)
 
@@ -44,9 +55,11 @@ def main(argv=sys.argv[1:]):
     dist_file = dist_cache.distribution_file
 
     # determine source repositories for overlay workspace
-    packages = find_packages('catkin_workspace/src')
-    underlay_package_names = [pkg.name for pkg in packages.values()]
-    print("Underlay workspace contains %d packages:%s" %
+    underlay_package_names = args.underlay_packages
+    if underlay_package_names is None:
+        packages = find_packages('catkin_workspace/src')
+        underlay_package_names = [pkg.name for pkg in packages.values()]
+    print('Underlay workspace contains %d packages:%s' %
           (len(underlay_package_names),
            ''.join(['\n- %s' % pkg_name
                     for pkg_name in sorted(underlay_package_names)])),
@@ -55,7 +68,7 @@ def main(argv=sys.argv[1:]):
     overlay_package_names = get_overlay_package_names(
         args.pkg, args.exclude_pkg, args.level,
         underlay_package_names, dist_cache.release_package_xmls, output=True)
-    print("Overlay workspace will contain %d packages:%s" %
+    print('Overlay workspace will contain %d packages:%s' %
           (len(overlay_package_names),
            ''.join(['\n- %s' % pkg_name
                     for pkg_name in sorted(overlay_package_names)])),
@@ -70,23 +83,26 @@ def main(argv=sys.argv[1:]):
         (repositories[k], 'catkin_workspace_overlay/src/%s' % k)
         for k in sorted(repositories.keys())]
 
-    value = expand_template(
-        'prerelease/prerelease_overlay_script.sh.em', {
-            'scms': scms},
-        options={BANGPATH_OPT: False})
-    print(value)
+    if not args.json:
+        value = expand_template(
+            'prerelease/prerelease_overlay_script.sh.em', {
+                'scms': scms},
+            options={BANGPATH_OPT: False})
+        print(value)
+    else:
+        print(json.dumps([vars(r) for r, p in scms], sort_keys=True, indent=2))
 
 
 def get_repository_specification_for_released_package(dist_file, pkg_name):
-        repo_name = dist_file.release_packages[pkg_name].repository_name
-        release_repo = dist_file.repositories[repo_name].release_repository
-        version_tag = get_release_tag(release_repo, pkg_name)
-        return RepositorySpecification(
-            pkg_name, {
-                'type': 'git',
-                'url': release_repo.url,
-                'version': version_tag,
-            })
+    repo_name = dist_file.release_packages[pkg_name].repository_name
+    release_repo = dist_file.repositories[repo_name].release_repository
+    version_tag = get_release_tag(release_repo, pkg_name)
+    return RepositorySpecification(
+        pkg_name, {
+            'type': 'git',
+            'url': release_repo.url,
+            'version': version_tag,
+        })
 
 
 if __name__ == '__main__':
