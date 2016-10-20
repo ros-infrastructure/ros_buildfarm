24198ee0068a3bc94d90989523cd488331bc1fe9
Dirk Thomas <dthomas@osrfoundation.org>
2016-10-20 11:21:48 -0700

    1.2.1

--- ros_buildfarm/_version.py
+++ ros_buildfarm/_version.py
@@ -14,4 +14,4 @@
 
 # the version must be either a released version number which is also a tag name
 # or an upcoming version number followed by a branch name separated by a dash
-__version__ = '1.2.1-master'
+__version__ = '1.2.1'
