90cdef2799be8951bfb8f746b095b4825b41f372
Dirk Thomas <dthomas@osrfoundation.org>
2016-11-29 16:04:27 -0800

    use flake8-import-order

--- test/test_flake8.py
+++ test/test_flake8.py
@@ -14,25 +14,42 @@
 
 import os
 
-from flake8.api.legacy import get_style_guide
+from flake8.api.legacy import StyleGuide
+from flake8.main.application import Application
 
 
 def test_flake8_conformance():
     """Test source code for flake8 conformance."""
-    style_guide = get_style_guide(
-        ignore=[
+    argv = [
+        '--ignore=%s' % ','.join([
             'D100', 'D101', 'D102', 'D103', 'D104', 'D105',
-            'E501',
-        ],
-    )
+            'E501']),
+        '--import-order-style=google',
+    ]
+    style_guide = get_style_guide(argv)
     base_path = os.path.join(os.path.dirname(__file__), '..')
     paths = [
         os.path.join(base_path, 'ros_buildfarm'),
         os.path.join(base_path, 'scripts'),
+        os.path.join(base_path, 'test'),
     ]
     report = style_guide.check_files(paths)
     assert report.total_errors == 0, \
         'Found %d code style warnings' % report.total_errors
+
+
+def get_style_guide(argv=None):
+    # this is a fork of flake8.api.legacy.get_style_guide
+    # to allow passing command line argument
+    application = Application()
+    application.find_plugins()
+    application.register_plugin_options()
+    application.parse_configuration_and_cli(argv)
+    application.make_formatter()
+    application.make_notifier()
+    application.make_guide()
+    application.make_file_checker_manager()
+    return StyleGuide(application)
 
 
 if __name__ == '__main__':
