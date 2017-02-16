9a3e2d8883a390f7b660bbaa328c1b0559b967b4
Dirk Thomas <dthomas@osrfoundation.org>
2017-02-16 14:33:08 -0800

    override compression level on Zesty

--- scripts/release/build_sourcedeb.py
+++ scripts/release/build_sourcedeb.py
@@ -17,6 +17,8 @@
 import argparse
 import sys
 
+from ros_buildfarm.argument import add_argument_os_code_name
+from ros_buildfarm.argument import add_argument_os_name
 from ros_buildfarm.argument import add_argument_source_dir
 from ros_buildfarm.common import Scope
 from ros_buildfarm.sourcedeb_job import build_sourcedeb
@@ -26,10 +28,14 @@ def main(argv=sys.argv[1:]):
     with Scope('SUBSECTION', 'build sourcedeb'):
         parser = argparse.ArgumentParser(
             description='Build package sourcedeb')
+        add_argument_os_name(parser)
+        add_argument_os_code_name(parser)
         add_argument_source_dir(parser)
         args = parser.parse_args(argv)
 
-        return build_sourcedeb(args.source_dir)
+        return build_sourcedeb(
+            args.source_dir,
+            os_name=args.os_name, os_code_name=args.os_code_name)
 
 
 if __name__ == '__main__':
