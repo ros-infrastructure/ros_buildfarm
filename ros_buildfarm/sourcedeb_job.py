d265039574ff3ea28ae33ef7973436a9b29e00b7
Dirk Thomas <dthomas@osrfoundation.org>
2017-03-08 12:17:41 -0800

    fix spelling

--- ros_buildfarm/sourcedeb_job.py
+++ ros_buildfarm/sourcedeb_job.py
@@ -117,7 +117,7 @@ def build_sourcedeb(sources_dir, os_name=None, os_code_name=None):
         # dpkg-buildpackage args
         '-us', '-uc',
         # set the option for dpkg-source to auto-commit upstream changes
-        # This is needed for debian incrments where the upstream had changed.
+        # This is needed for Debian increments where the upstream has changed.
         # It's not the best practice but we have people doing it a bunch.
         '--hook-source=\'bash -c "echo auto-commit > debian/source/options"\'',
         # debuild args for lintian
