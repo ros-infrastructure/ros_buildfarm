00a867a8813c78c9f402ae4d7f4c6e7535f0a4f7
Dirk Thomas <dthomas@osrfoundation.org>
2017-03-14 13:41:11 -0700

    split Debian package into scripts and modules

--- setup.py
+++ setup.py
@@ -3,48 +3,49 @@ import os
 from setuptools import find_packages
 from setuptools import setup
 
-# get version number from module
-version_file = os.path.join(
-    os.path.dirname(__file__), 'ros_buildfarm', '_version.py')
-with open(version_file) as h:
-    exec(h.read())
-
 # Get a list of scripts to install
 scripts = []
-for root, dirnames, filenames in os.walk('scripts'):
+for root, _, filenames in os.walk('scripts'):
     for filename in filenames:
         scripts.append(os.path.join(root, filename))
-
-install_requires = [
-    'catkin-pkg >= 0.2.6',
-    'empy',
-    'PyYAML',
-    'rosdistro >= 0.4.0',
-]
-if os.sys.version_info.major == 2:
-    install_requires.append('configparser')
 
 # Get the long description out of the readme.md
 with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
     long_description = f.read()
 
-setup(
-    name='ros_buildfarm',
-    version=__version__,
-    packages=find_packages(exclude=['test']),
-    scripts=scripts,
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=install_requires,
-    author='Dirk Thomas',
-    author_email='dthomas@osrfoundation.org',
-    maintainer='Dirk Thomas',
-    maintainer_email='dthomas@osrfoundation.org',
-    url='https://github.com/ros-infrastructure/ros_buildfarm',
-    keywords=['ROS', 'buildfarm', 'catkin'],
-    classifiers=['Programming Language :: Python',
-                 'License :: OSI Approved :: Apache Software License'],
-    description="Build farm used to build the ROS ecosystem's packages.",
-    long_description=long_description,
-    license='Apache 2.0',
-)
+kwargs = {
+    'name': 'ros_buildfarm',
+    'version': '1.2.2-master',  # same version as in ros_buildfarm/__init__.py
+    'packages': find_packages(exclude=['test']),
+    'scripts': scripts,
+    'include_package_data': True,
+    'zip_safe': False,
+    'install_requires': [
+        'empy',
+        'PyYAML'],
+    'author': 'Dirk Thomas',
+    'author_email': 'dthomas@osrfoundation.org',
+    'maintainer': 'Dirk Thomas',
+    'maintainer_email': 'dthomas@osrfoundation.org',
+    'url': 'https://github.com/ros-infrastructure/ros_buildfarm',
+    'keywords': ['ROS', 'buildfarm', 'catkin'],
+    'classifiers': [
+        'Programming Language :: Python',
+        'License :: OSI Approved :: Apache Software License'],
+    'description': "Build farm used to build the ROS ecosystem's packages.",
+    'long_description': long_description,
+    'license': 'Apache 2.0',
+}
+
+if os.sys.version_info.major == 2:
+    kwargs['install_requires'].append('configparser')
+
+if 'SKIP_PYTHON_MODULES' in os.environ:
+    kwargs['packages'] = []
+elif 'SKIP_PYTHON_SCRIPTS' in os.environ:
+    kwargs['name'] += '_modules'
+    kwargs['scripts'] = []
+else:
+    kwargs['install_requires'] += ['catkin_pkg >= 0.2.6', 'rosdistro >= 0.4.0']
+
+setup(**kwargs)
