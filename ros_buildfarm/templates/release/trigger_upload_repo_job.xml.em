bba87b48054a8580933ae0a8eb24ff318cafb00a
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-09-05 10:17:53 -0400

    Add initial version of upload trigger job generators.
    This adds a template and generating script for the upload trigger jobs.
    On the current farm as `upload_main` and `upload_testing`.

---
+++ ros_buildfarm/templates/release/trigger_upload_repo_job.xml.em
@@ -0,0 +1,42 @@
+<project>
+  <actions/>
+  <description/>
+  <keepDependencies>false</keepDependencies>
+  <properties>
+    <jenkins.advancedqueue.priority.strategy.PriorityJobProperty plugin="PrioritySorter@@3.5.0">
+      <useJobPriority>false</useJobPriority>
+      <priority>-1</priority>
+    </jenkins.advancedqueue.priority.strategy.PriorityJobProperty>
+    <org.jenkinsci.plugins.requeuejob.RequeueJobProperty plugin="jobrequeue@@1.1">
+      <requeueJob>false</requeueJob>
+    </org.jenkinsci.plugins.requeuejob.RequeueJobProperty>
+  </properties>
+  <scm class="hudson.scm.NullSCM"/>
+  <assignedNode>building_repository</assignedNode>
+  <canRoam>false</canRoam>
+  <disabled>false</disabled>
+  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
+  <blockBuildWhenUpstreamBuilding>true</blockBuildWhenUpstreamBuilding>
+  <triggers>
+    <jenkins.triggers.ReverseBuildTrigger>
+      <spec/>
+      <upstreamProjects>@upstream_job_names</upstreamProjects>
+      <threshold>
+        <name>SUCCESS</name>
+        <ordinal>0</ordinal>
+        <color>BLUE</color>
+        <completeBuild>true</completeBuild>
+      </threshold>
+    </jenkins.triggers.ReverseBuildTrigger>
+  </triggers>
+  <concurrentBuild>false</concurrentBuild>
+  <builders>
+    <hudson.tasks.Shell>
+      <command>#!/bin/bash
+$HOME/upload_triggers/upload_repo.bash @repo
+    </command>
+    </hudson.tasks.Shell>
+  </builders>
+  <publishers/>
+  <buildWrappers/>
+</project>
---
+++ scripts/release/generate_release_trigger_upload_jobs.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+# Copyright 2017 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import sys
+
+from ros_buildfarm.argument import add_argument_config_url
+from ros_buildfarm.argument import add_argument_dry_run
+from ros_buildfarm.common import get_release_job_prefix
+from ros_buildfarm.config import get_index
+from ros_buildfarm.jenkins import configure_job
+from ros_buildfarm.jenkins import connect
+from ros_buildfarm.templates import expand_template
+
+
+def main(argv=sys.argv[1:]):
+    parser = argparse.ArgumentParser(
+        description="Generate the 'upload_main' and 'upload_testing' jobs.")
+    add_argument_config_url(parser)
+    add_argument_dry_run(parser)
+    args = parser.parse_args(argv)
+
+    template_name = 'release/trigger_upload_repo_job.xml.em'
+
+    config = get_index(args.config_url)
+    jenkins = connect(config.jenkins_url)
+    distributions = config.distributions.keys()
+
+    for repo in ['main', 'testing']:
+        job_name = 'upload_%s' % repo
+        upstream_job_names = ['{0}_sync-packages-to-{1}'.format(
+            get_release_job_prefix(rosdistro), repo) for rosdistro in distributions]
+        upstream_job_names = ', '.join(upstream_job_names)
+        job_config = expand_template(template_name, {
+            'repo': repo,
+            'upstream_job_names': upstream_job_names})
+
+        configure_job(jenkins, job_name, job_config, dry_run=args.dry_run)
+
+
+if __name__ == '__main__':
+    main()
