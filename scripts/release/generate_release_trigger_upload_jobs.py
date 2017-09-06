73b88a3887371728ca30af06267fe1b04e006548
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-09-06 11:08:11 -0400

    Generate correct upstream jobs for testing repositories.
    I couldn't determine a more efficient way to procedurally generate all
    such jobs from the config files. Another option would be to query the
    Jenkins instance for a list of matching job names.

--- scripts/release/generate_release_trigger_upload_jobs.py
+++ scripts/release/generate_release_trigger_upload_jobs.py
@@ -19,8 +19,10 @@ import sys
 
 from ros_buildfarm.argument import add_argument_config_url
 from ros_buildfarm.argument import add_argument_dry_run
+from ros_buildfarm.common import JobValidationError
 from ros_buildfarm.common import get_release_job_prefix
 from ros_buildfarm.config import get_index
+from ros_buildfarm.config import get_release_build_files
 from ros_buildfarm.jenkins import configure_job
 from ros_buildfarm.jenkins import connect
 from ros_buildfarm.templates import expand_template
@@ -37,22 +39,49 @@ def main(argv=sys.argv[1:]):
 
     config = get_index(args.config_url)
     jenkins = connect(config.jenkins_url)
-    distributions = config.distributions.keys()
 
     for repo in ['main', 'testing']:
         job_name = 'upload_%s' % repo
         block_when_upstream_building = 'true'
         if repo == 'testing':
             block_when_upstream_building = 'false'
-        upstream_job_names = ['{0}_sync-packages-to-{1}'.format(
-            get_release_job_prefix(rosdistro), repo) for rosdistro in distributions]
-        upstream_job_names = ', '.join(sorted(upstream_job_names))
         job_config = expand_template(template_name, {
             'block_when_upstream_building': block_when_upstream_building,
             'repo': repo,
-            'upstream_job_names': upstream_job_names})
+            'upstream_job_names': get_upstream_job_names(config, repo)})
 
         configure_job(jenkins, job_name, job_config, dry_run=args.dry_run)
+
+
+def get_upstream_job_names(config, repo):
+    distributions = config.distributions.keys()
+    if repo == 'main':
+        upstream_job_names = ['{0}_sync-packages-to-{1}'.format(
+            get_release_job_prefix(rosdistro), repo) for rosdistro in distributions]
+    elif repo == 'testing':
+        upstream_job_names = []
+        for rosdistro in distributions:
+            architectures_by_code_name = {}
+            build_files = get_release_build_files(config, rosdistro)
+            for k, build_file in build_files.items():
+                for os_name in build_file.targets.keys():
+                    for code_name, architectures in build_file.targets[os_name].items():
+                        architectures_by_code_name[code_name] = \
+                            set(architectures_by_code_name.get(code_name, set()).union(
+                                set(architectures.keys())))
+
+            for code_name, archs in architectures_by_code_name.items():
+                for arch in archs:
+                    upstream_job_names.append(
+                        '{prefix}_sync-packages-to-{repo}_{codename}_{arch}'.format(
+                            prefix=get_release_job_prefix(rosdistro),
+                            repo=repo,
+                            codename=code_name,
+                            arch=arch))
+    else:
+        raise JobValidationError("Unknown upstream jobs for job 'upload_{}'." % repo)
+    upstream_job_names = ','.join(sorted(upstream_job_names))
+    return upstream_job_names
 
 
 if __name__ == '__main__':
