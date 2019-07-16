ros-infrastructure/ros_buildfarm

commit 6906c26a2f9412aa79cd85d30ac2358bbd723054

Fix CI build detection of non-ROS packages (#642)
During the dependency walk, any ROS packages encountered as a dependency 
were recursively walked, and therefore aren't included in the resulting
enumeration. Beyond that, we remove any keys which have been
specifically ignored (via `skip_rosdep_keys`). Another quirk was added
such that if a package is present but was specifically ignored, we also
exclude it from the dependencies to avoid installing a deb package to
satisfy the dependency.

The change is to take that a step further and remove dependencies on any 
non-ROS packages present in the workspaces. Instead of specifically
enumerating the non-ROS packages, I'm just removing any package (ROS,
non-ROS, ignored, etc) present in the workspace.

The new behavior should be that the only time a ROS deb is installed is 
if the package listed as a dependency isn't part of ANY of the
workspaces AND the package isn't specifically ignored via
`skip_rosdep_keys`.

scripts/ci/generate_install_lists.py
@@ -58,7 +58,7 @@ def main(argv=sys.argv[1:]):
     os.chdir(workspace_root)
 
     with Scope('SUBSECTION', 'mark packages with IGNORE files'):
-        packages = locate_packages(workspace_root)
+        all_packages = locate_packages(workspace_root)
         if args.package_selection_args:
             print(
                 'Using package selection arguments:',
@@ -66,25 +66,27 @@ def main(argv=sys.argv[1:]):
             selected_packages = locate_packages(
                 workspace_root, extra_args=args.package_selection_args)
 
-            to_ignore = packages.keys() - selected_packages.keys()
+            to_ignore = all_packages.keys() - selected_packages.keys()
             print('Ignoring %d packages' % len(to_ignore))
             for package in sorted(to_ignore):
                 print('-', package)
-                package_root = packages.pop(package)
+                package_root = all_packages[package]
                 Path(package_root, 'COLCON_IGNORE').touch()
 
         print('There are %d packages which meet selection criteria' %
-              len(packages))
+              len(selected_packages))
 
     with Scope('SUBSECTION', 'Enumerating packages needed to build'):
         # find all of the underlay packages
         underlay_pkgs = {}
+        all_underlay_pkg_names = set()
         for package_root in args.package_root[0:-1]:
             print("Crawling for packages in '%s'" % package_root)
             underlay_pkgs.update(find_packages(package_root))
+            all_underlay_pkg_names |= locate_packages(package_root).keys()
 
         underlay_pkg_names = [pkg.name for pkg in underlay_pkgs.values()]
-        print('Found the following underlay packages:')
+        print('Found the following ROS underlay packages:')
         for pkg_name in sorted(underlay_pkg_names):
             print('  -', pkg_name)
 
@@ -94,7 +96,7 @@ def main(argv=sys.argv[1:]):
         pkgs = find_packages(package_root)
 
         pkg_names = [pkg.name for pkg in pkgs.values()]
-        print('Found the following packages:')
+        print('Found the following ROS packages:')
         for pkg_name in sorted(pkg_names):
             print('  -', pkg_name)
 
@@ -119,9 +121,12 @@ def main(argv=sys.argv[1:]):
         if args.skip_rosdep_keys:
             dependency_keys_build.difference_update(args.skip_rosdep_keys)
             dependency_keys_test.difference_update(args.skip_rosdep_keys)
-        if args.package_selection_args:
-            dependency_keys_build.difference_update(to_ignore)
-            dependency_keys_test.difference_update(to_ignore)
+
+        # remove all non-ROS packages and packages which are present but
+        # specifically ignored
+        every_package_name = all_packages.keys() | all_underlay_pkg_names
+        dependency_keys_build -= every_package_name
+        dependency_keys_test -= every_package_name
 
         context = initialize_resolver(
             args.rosdistro_name, args.os_name, args.os_code_name)

commit dfd379eb6e8e5148cc85aabfbcfa20e1d97cdd3e

Fix a regression in CI builds with no package selection args
Regression caused by 6906c26a2f9412aa79cd85d30ac2358bbd723054

scripts/ci/generate_install_lists.py
@@ -59,6 +59,7 @@ def main(argv=sys.argv[1:]):
 
     with Scope('SUBSECTION', 'mark packages with IGNORE files'):
         all_packages = locate_packages(workspace_root)
+        selected_packages = all_packages
         if args.package_selection_args:
             print(
                 'Using package selection arguments:',