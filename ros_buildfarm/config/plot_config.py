3feff0eaa30cf67953eded1df4a7b89123eeffa6
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2020-01-15 09:30:50 -0800

    simplify num_builds logic

--- ros_buildfarm/config/plot_config.py
+++ ros_buildfarm/config/plot_config.py
@@ -46,9 +46,8 @@ class PlotConfig:
         self.y_axis_minimum = data.get('y_axis_minimum', None)
         self.y_axis_maximum = data.get('y_axis_maximum', None)
 
-        self.num_builds = ""
-        if 'num_builds' in data:
-            self.num_builds = data['num_builds']
+        self.num_builds = data.get('num_builds', 0)
+        assert isinstance(self.num_builds, int)
 
         self.data_series = []
         if 'data_series' in data:
