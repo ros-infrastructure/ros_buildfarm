0dcd7ce135ea33ab66a39243c070f557a448b96b
Scott K Logan <logans@cottsay.net>
2020-01-15 17:46:58 -0800

    Refactor download and caching out of debian_repo.py

--- ros_buildfarm/debian_repo.py
+++ ros_buildfarm/debian_repo.py
@@ -12,21 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from gzip import GzipFile
-import hashlib
-from io import BytesIO
 import logging
 import os
-import socket
-import time
-try:
-    from urllib.error import HTTPError
-    from urllib.error import URLError
-    from urllib.request import urlopen
-except ImportError:
-    from urllib2 import HTTPError
-    from urllib2 import URLError
-    from urllib2 import urlopen
+
+from .package_repo import fetch_and_cache_gzip
 
 
 def get_debian_repo_data(debian_repository_baseurl, targets, cache_dir):
@@ -46,10 +35,7 @@ def get_debian_repo_index(debian_repository_baseurl, target, cache_dir):
     else:
         url = os.path.join(url, 'binary-%s' % target.arch, 'Packages.gz')
 
-    cache_filename = os.path.join(
-        cache_dir, hashlib.md5(url.encode()).hexdigest())
-    if not os.path.exists(cache_filename):
-        fetch_gzip_url(url, cache_filename)
+    cache_filename = fetch_and_cache_gzip(url, cache_dir)
 
     logging.debug('Reading file: %s' % cache_filename)
     # split package blocks
@@ -71,36 +57,3 @@ def get_debian_repo_index(debian_repository_baseurl, target, cache_dir):
         package_versions[debian_pkg_name] = version
 
     return package_versions
-
-
-def fetch_gzip_url(url, dst_filename):
-    dst_dirname = os.path.dirname(dst_filename)
-    if not os.path.exists(dst_dirname):
-        os.makedirs(dst_dirname)
-    logging.debug('Downloading gz url: %s' % url)
-    gz_str = load_url(url)
-    gz_stream = BytesIO(gz_str)
-    g = GzipFile(fileobj=gz_stream, mode='rb')
-    with open(dst_filename, 'wb') as f:
-        f.write(g.read())
-
-
-def load_url(url, retry=2, retry_period=1, timeout=10):
-    try:
-        fh = urlopen(url, timeout=timeout)
-    except HTTPError as e:
-        if e.code == 503 and retry:
-            time.sleep(retry_period)
-            return load_url(
-                url, retry=retry - 1, retry_period=retry_period,
-                timeout=timeout)
-        e.msg += ' (%s)' % url
-        raise
-    except URLError as e:
-        if isinstance(e.reason, socket.timeout) and retry:
-            time.sleep(retry_period)
-            return load_url(
-                url, retry=retry - 1, retry_period=retry_period,
-                timeout=timeout)
-        raise URLError(str(e) + ' (%s)' % url)
-    return fh.read()
