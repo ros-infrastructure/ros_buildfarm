91a85182f994a5bce657901f60771ce1ccbfcc9d
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2020-01-30 20:12:24 -0800

    revert custom xunit types

--- ros_buildfarm/devel_job.py
+++ ros_buildfarm/devel_job.py
@@ -437,8 +437,7 @@ def _get_devel_job_config(
 
         # only Ubuntu Focal has a new enough pytest version which generates
         # JUnit compliant result files
-        'xunit_publisher_types': get_xunit_publisher_types_and_patterns(
-            ros_version, os_name == 'ubuntu' and os_code_name != 'bionic'),
+        'xunit_publisher_types': get_xunit_publisher_types_and_patterns(),
 
         'git_ssh_credential_id': config.git_ssh_credential_id,
 
