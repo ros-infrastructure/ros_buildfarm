ros-infrastructure/ros_buildfarm

commit 7719e9b26a73ca6878050709b51afced67b1ecca

Process conditional dependencies in release jobs (#758)
This change adds the same conditional variables as are used during
blooming, so a conditional dependency on a ROS package should now be
possible.

ros_buildfarm/release_job.py
@@ -24,6 +24,7 @@ from ros_buildfarm.common import get_github_project_url
 from ros_buildfarm.common import get_implicitly_ignored_package_names
 from ros_buildfarm.common import get_node_label
 from ros_buildfarm.common import get_os_package_name
+from ros_buildfarm.common import get_package_condition_context
 from ros_buildfarm.common import get_release_binary_view_name
 from ros_buildfarm.common import get_release_job_prefix
 from ros_buildfarm.common import get_release_source_view_name
@@ -321,6 +322,10 @@ def _get_and_parse_distribution_cache(index, rosdistro_name, pkg_names):
         for pkg_name, pkg_xml in dist_cache.release_package_xmls.items()
         if pkg_name in pkg_names
     }
+
+    condition_context = get_package_condition_context(index, rosdistro_name)
+    for pkg in cached_pkgs.values():
+        pkg.evaluate_conditions(condition_context)
 
     # for ROS 2 distributions bloom injects a dependency on ros_workspace
     # into almost all packages (except its dependencies)

ros_buildfarm/common.py
@@ -509,7 +509,8 @@ def topological_order_packages(packages):
             decorator.package.run_depends + decorator.package.test_depends
         # skip external dependencies, meaning names that are not known packages
         unique_depend_names = set([
-            d.name for d in all_depends if d.name in decorators_by_name.keys()])
+            d.name for d in all_depends if d.name in decorators_by_name.keys() and
+            d.evaluated_condition is not False])
         for name in unique_depend_names:
             if name in decorator.depends_for_topological_order:
                 # avoid function call to improve performance
@@ -613,7 +614,8 @@ def get_direct_dependencies(pkg_name, cached_pkgs, pkg_names):
             pkg.build_export_depends +
             pkg.exec_depends +
             pkg.test_depends)
-        if d.name in pkg_names])
+        if d.name in pkg_names and
+        d.evaluated_condition is not False])
     return depends
 
 

commit fd8e6dd60b60476db2728b73ac895e1e92b8cf10

Extract empty group dependencies for topological ordering.
This is a fix for an issue introduced by #758. Now that conditional
dependencies are being evaluated and checked, an assert is triggering
during release jobs since group memberships are not being extracted.

However, if we were to extract true group memberships we could create a
discrepancy between the topological order according to ros_buildfarm and
the dependency tree used by bloom as bloom does not currently support
group dependencies.

ros_buildfarm/release_job.py
@@ -152,6 +152,15 @@ def configure_release_jobs(
     for pkg_name in set(pkg_names).difference(cached_pkgs.keys()):
         print("Skipping package '%s': no released package.xml in cache" %
               (pkg_name), file=sys.stderr)
+    # topological_order_packages will assert if the members attribute of any
+    # group dependency is None. This works around this by extracting group
+    # members from an empty set. This is preferable to honoring group
+    # dependencies as bloom does not currently support them so this matches
+    # that behavior.
+    for pkg, pkgdata in cached_pkgs:
+        if pkgdata.group_depends:
+            for group_depend in pkgdata.group_depends:
+                group_depend.extract_group_members({})
     ordered_pkg_tuples = topological_order_packages(cached_pkgs)
 
     other_build_files = [v for k, v in build_files.items() if k != release_build_name]