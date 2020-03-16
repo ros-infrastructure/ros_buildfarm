8e5a8c45ac5818443e2fadd8b70262aa65b0cd30
Scott K Logan <logans@cottsay.net>
2020-03-16 15:34:20 -0700

    Create a standalone script for performing pulp repo syncs
    Also add a DRY_RUN option to import jobs and fix a small typo in the
    console output.

--- scripts/release/rpm/import_package.py
+++ scripts/release/rpm/import_package.py
@@ -17,6 +17,7 @@
 import argparse
 import sys
 
+from ros_buildfarm.argument import add_argument_dry_run
 from ros_buildfarm.argument import add_argument_invalidate
 from ros_buildfarm.argument import add_argument_pulp_base_url
 from ros_buildfarm.argument import add_argument_pulp_distribution_name
@@ -35,6 +36,7 @@ def main(argv=sys.argv[1:]):
         'package_resources',
         nargs='*', metavar="PULP_HREF",
         help='Identifiers for packages which should be imported')
+    add_argument_dry_run(parser)
     add_argument_invalidate(parser)
     parser.add_argument(
         '--invalidate-expression',
@@ -54,11 +56,11 @@ def main(argv=sys.argv[1:]):
     with Scope('SUBSECTION', 'performing repository transaction'):
         pkgs_added, pkgs_removed = pulp_client.import_and_invalidate(
             args.pulp_distribution_name, args.package_resources,
-            args.invalidate_expression, args.invalidate)
+            args.invalidate_expression, args.invalidate, dry_run=args.dry_run)
 
     with Scope('SUBSECTION', 'enumerating results'):
         if not pkgs_added:
-            print('Not importing any new packages)')
+            print('Not importing any new packages')
         for pkg in pkgs_added:
             print('Importing package: %s-%s.%s' % (pkg.name, format_pkg_ver(pkg), pkg.arch))
 
