543dfccc25c7f8a07c49f60480fe440b6e4d2f11
Scott K Logan <logans@cottsay.net>
2021-08-24 16:53:23 -0700

    Add package_dependecy_behavior controls for release builds

--- ros_buildfarm/config/release_build_file.py
+++ ros_buildfarm/config/release_build_file.py
@@ -108,6 +108,16 @@ class ReleaseBuildFile(BuildFile):
         if 'upload_destination_credential_id' in data:
             self.upload_destination_credential_id = data['upload_destination_credential_id']
 
+        self.include_test_dependencies = True
+        self.run_package_tests = True
+        if 'package_dependency_behavior' in data:
+            if 'include_test_dependencies' in data['package_dependency_behavior']:
+                self.include_test_dependencies = \
+                    bool(data['package_dependency_behavior']['include_test_dependencies'])
+            if 'run_package_tests' in data['package_dependency_behavior']:
+                self.run_package_tests = \
+                    bool(data['package_dependency_behavior']['run_package_tests'])
+
     def filter_packages(self, package_names):
         res = set(package_names)
         if self.package_whitelist:
