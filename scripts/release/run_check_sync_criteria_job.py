ros-infrastructure/ros_buildfarm

commit 9b26a191d3e43d91a2bd101c6848314b828f7de0

Add os_name argument to check_sync_criteria (#726)
Also use the get_package_repo_data method in place of the current
debian-specific one.

scripts/release/run_check_sync_criteria_job.py
@@ -27,6 +27,7 @@ from ros_buildfarm.argument import \
 from ros_buildfarm.argument import add_argument_distribution_repository_urls
 from ros_buildfarm.argument import add_argument_dockerfile_dir
 from ros_buildfarm.argument import add_argument_os_code_name
+from ros_buildfarm.argument import add_argument_os_name
 from ros_buildfarm.argument import add_argument_rosdistro_name
 from ros_buildfarm.common import get_distribution_repository_keys
 from ros_buildfarm.common import get_user_id
@@ -39,6 +40,10 @@ def main(argv=sys.argv[1:]):
     add_argument_config_url(parser)
     add_argument_rosdistro_name(parser)
     add_argument_build_name(parser, 'release')
+    parser.add_argument(
+        'os_name', nargs='?',
+        help='An OS name from the build file')
+    add_argument_os_name(parser)
     add_argument_os_code_name(parser)
     add_argument_arch(parser)
     add_argument_distribution_repository_urls(parser)
@@ -46,6 +51,12 @@ def main(argv=sys.argv[1:]):
     add_argument_cache_dir(parser)
     add_argument_dockerfile_dir(parser)
     args = parser.parse_args(argv)
+
+    if args.os_name is None:
+        print(
+            'WARNING: Calling %s without specifying os_name is deprecated' % argv[0],
+            file=sys.stderr)
+        args.os_name = 'ubuntu'
 
     data = copy.deepcopy(args.__dict__)
     data.update({

ros_buildfarm/release_job.py
@@ -140,7 +140,7 @@ def configure_release_jobs(
         for arch in sorted(build_file.targets[os_name][os_code_name]):
             job_name, job_config = configure_sync_packages_to_testing_job(
                 config_url, rosdistro_name, release_build_name,
-                os_code_name, arch,
+                os_name, os_code_name, arch,
                 config=config, build_file=build_file, jenkins=jenkins,
                 dry_run=dry_run)
             if not jenkins:
@@ -458,7 +458,7 @@ def configure_release_job(
         for arch in build_file.targets[os_name][os_code_name]:
             configure_sync_packages_to_testing_job(
                 config_url, rosdistro_name, release_build_name,
-                os_code_name, arch,
+                os_name, os_code_name, arch,
                 config=config, build_file=build_file, jenkins=jenkins,
                 dry_run=dry_run)
 
@@ -781,8 +781,8 @@ def _get_import_package_job_config(build_file):
 
 
 def configure_sync_packages_to_testing_job(
-        config_url, rosdistro_name, release_build_name, os_code_name, arch,
-        config=None, build_file=None, jenkins=None, dry_run=False):
+        config_url, rosdistro_name, release_build_name, os_name, os_code_name,
+        arch, config=None, build_file=None, jenkins=None, dry_run=False):
     if config is None:
         config = get_config_index(config_url)
     if build_file is None:
@@ -795,8 +795,8 @@ def configure_sync_packages_to_testing_job(
     job_name = get_sync_packages_to_testing_job_name(
         rosdistro_name, os_code_name, arch)
     job_config = _get_sync_packages_to_testing_job_config(
-        config_url, rosdistro_name, release_build_name, os_code_name, arch,
-        config, build_file)
+        config_url, rosdistro_name, release_build_name, os_name, os_code_name,
+        arch, config, build_file)
 
     # jenkinsapi.jenkins.Jenkins evaluates to false if job count is zero
     if isinstance(jenkins, object) and jenkins is not False:
@@ -813,8 +813,8 @@ def get_sync_packages_to_testing_job_name(
 
 
 def _get_sync_packages_to_testing_job_config(
-        config_url, rosdistro_name, release_build_name, os_code_name, arch,
-        config, build_file):
+        config_url, rosdistro_name, release_build_name, os_name, os_code_name,
+        arch, config, build_file):
     template_name = 'release/deb/sync_packages_to_testing_job.xml.em'
 
     repository_args, script_generating_key_files = \
@@ -828,6 +828,7 @@ def _get_sync_packages_to_testing_job_config(
         'config_url': config_url,
         'rosdistro_name': rosdistro_name,
         'release_build_name': release_build_name,
+        'os_name': os_name,
         'os_code_name': os_code_name,
         'arch': arch,
         'repository_args': repository_args,

ros_buildfarm/templates/release/deb/sync_packages_to_testing_job.xml.em
@@ -59,6 +59,7 @@
         ' ' + config_url +
         ' ' + rosdistro_name +
         ' ' + release_build_name +
+        ' ' + os_name +
         ' ' + os_code_name +
         ' ' + arch +
         ' ' + ' '.join(repository_args) +

ros_buildfarm/templates/release/release_check_sync_criteria_task.Dockerfile.em
@@ -47,6 +47,7 @@ cmd = \
     ' ' + config_url + \
     ' ' + rosdistro_name + \
     ' ' + release_build_name + \
+    ' ' + os_name + \
     ' ' + os_code_name + \
     ' ' + arch + \
     ' --cache-dir ' + cache_dir

scripts/release/check_sync_criteria.py
@@ -24,12 +24,13 @@ from ros_buildfarm.argument import add_argument_build_name
 from ros_buildfarm.argument import add_argument_cache_dir
 from ros_buildfarm.argument import add_argument_config_url
 from ros_buildfarm.argument import add_argument_os_code_name
+from ros_buildfarm.argument import add_argument_os_name
 from ros_buildfarm.argument import add_argument_rosdistro_name
 from ros_buildfarm.common import get_os_package_name
+from ros_buildfarm.common import get_package_repo_data
 from ros_buildfarm.common import Target
 from ros_buildfarm.config import get_index as get_config_index
 from ros_buildfarm.config import get_release_build_files
-from ros_buildfarm.debian_repo import get_debian_repo_index
 from rosdistro import get_distribution_file
 from rosdistro import get_index
 
@@ -41,6 +42,7 @@ def main(argv=sys.argv[1:]):
     add_argument_config_url(parser)
     add_argument_rosdistro_name(parser)
     add_argument_build_name(parser, 'release')
+    add_argument_os_name(parser)
     add_argument_os_code_name(parser)
     add_argument_arch(parser)
     add_argument_cache_dir(parser, '/tmp/package_repo_cache')
@@ -48,13 +50,13 @@ def main(argv=sys.argv[1:]):
 
     success = check_sync_criteria(
         args.config_url, args.rosdistro_name, args.release_build_name,
-        args.os_code_name, args.arch, args.cache_dir)
+        args.os_name, args.os_code_name, args.arch, args.cache_dir)
     return 0 if success else 1
 
 
 def check_sync_criteria(
-        config_url, rosdistro_name, release_build_name, os_code_name, arch,
-        cache_dir):
+        config_url, rosdistro_name, release_build_name, os_name,
+        os_code_name, arch, cache_dir):
     # fetch debian package list
     config = get_config_index(config_url)
     index = get_index(config.rosdistro_index_url)
@@ -62,10 +64,10 @@ def check_sync_criteria(
     build_files = get_release_build_files(config, rosdistro_name)
     build_file = build_files[release_build_name]
 
-    target = Target('ubuntu', os_code_name, arch)
+    target = Target(os_name, os_code_name, arch)
 
-    repo_index = get_debian_repo_index(
-        build_file.target_repository, target, cache_dir)
+    repo_index = get_package_repo_data(
+        build_file.target_repository, [target], cache_dir)[target]
 
     # for each release package which matches the release build file
     # check if a binary package exists

commit ad8c9293fec1ac25b30df2c7f369d6ed9e147a18

Fix sync on py2, fix PR builds, add sync and trigger CI
* Python 2 support for run_check_sync_criteria_job.py was regressed
  in #726.
* The wildcard pattern in the PR branch filter doesn't appear to work
  correctly when opening a PR between non-master branches.
* We've historically had no CI for trigger and sync jobs, but we're
  hitting some issues that are only happening in those job types (and
  reconfigure ones, but those are hard to test here).

.github/actions/sync_criteria_check/action.yaml
@@ -1,0 +1,55 @@
+---
+name: ros_buildfarm sync criteria check job
+description: Run a ROS buildfarm sync criteria check job
+inputs:
+  config_url:
+    description: URL of the buildfarm configuration index
+    required: true
+    default: https://raw.githubusercontent.com/ros-infrastructure/ros_buildfarm_config/production/index.yaml
+  config_name:
+    description: Name of the release configuration
+    required: true
+    default: default
+  ros_distro:
+    description: ROS distribution name
+    required: true
+  os_name:
+    description: Operating system name
+    required: true
+    default: ubuntu
+  os_code_name:
+    description: Operating system version name
+    required: true
+  arch:
+    description: Operating system architecture
+    required: true
+    default: amd64
+
+runs:
+  using: composite
+  steps:
+    - id: ros_buildfarm_job
+      shell: bash
+      run: |
+        echo ::group::Generate job
+        pushd $(mktemp -d)
+        mkdir -p docker_dir
+        curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc -o ros.asc
+        run_check_sync_criteria_job.py \
+          ${{inputs.config_url}} ${{inputs.ros_distro}} ${{inputs.config_name}} \
+          ${{inputs.os_name}} ${{inputs.os_code_name}} ${{inputs.arch}} \
+          --distribution-repository-urls http://repo.ros2.org/ubuntu/testing http://repositories.ros.org/ubuntu/testing \
+          --distribution-repository-key-files $PWD/ros.asc $PWD/ros.asc \
+          --cache-dir /tmp/package_repo_cache \
+          --dockerfile-dir $PWD/docker_dir
+        echo ::endgroup::
+
+        echo ::group::Build container
+        docker build --force-rm -t check_sync_condition $PWD/docker_dir
+        echo ::endgroup::
+
+        echo ::group::Run job
+        docker run --rm \
+          -v ${{github.workspace}}:/tmp/ros_buildfarm:ro \
+          check_sync_condition
+        echo ::endgroup

.github/actions/trigger/action.yaml
@@ -1,0 +1,45 @@
+---
+name: ros_buildfarm trigger job
+description: Run a ROS buildfarm trigger job (without actually triggering)
+inputs:
+  config_url:
+    description: URL of the buildfarm configuration index
+    required: true
+    default: https://raw.githubusercontent.com/ros-infrastructure/ros_buildfarm_config/production/index.yaml
+  config_name:
+    description: Name of the release configuration
+    required: true
+    default: default
+  ros_distro:
+    description: ROS distribution name
+    required: true
+
+runs:
+  using: composite
+  steps:
+    - id: ros_buildfarm_job
+      shell: bash
+      run: |
+        echo ::group::Generate job
+        pushd $(mktemp -d)
+        mkdir -p docker_dir trigger_jobs
+        curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc -o ros.asc
+        run_trigger_job.py \
+          ${{inputs.config_url}} ${{inputs.ros_distro}} ${{inputs.config_name}} \
+          --distribution-repository-urls http://repo.ros2.org/ubuntu/testing http://repositories.ros.org/ubuntu/testing \
+          --distribution-repository-key-files $PWD/ros.asc $PWD/ros.asc \
+          --groovy-script /tmp/trigger_jobs/trigger_jobs.groovy \
+          --cache-dir /tmp/package_repo_cache \
+          --dockerfile-dir $PWD/docker_dir
+        echo ::endgroup::
+
+        echo ::group::Build container
+        docker build --force-rm -t release_trigger_jobs $PWD/docker_dir
+        echo ::endgroup::
+
+        echo ::group::Run job
+        docker run --rm \
+          -v ${{github.workspace}}:/tmp/ros_buildfarm \
+          -v $PWD/trigger_jobs:/tmp/trigger_jobs \
+          release_trigger_jobs
+        echo ::endgroup

.github/workflows/ci.yaml
@@ -5,7 +5,6 @@ on:
   push:
     branches: [master]
   pull_request:
-    branches: ['*']
 
 jobs:
   pytest:
@@ -185,6 +184,39 @@ jobs:
         with:
           ros_distro: melodic
 
+  ros1_sync_criteria_check:
+    name: ROS 1 Sync Criteria Check
+    runs-on: ubuntu-18.04
+    strategy:
+      matrix:
+        python: ['2.7', '3.4', '3.5', '3.6']
+    steps:
+      - name: Check out project
+        uses: actions/checkout@v2
+      - name: Install dependencies
+        uses: ./.github/actions/setup
+      - name: Run job
+        uses: ./.github/actions/sync_criteria_check
+        with:
+          ros_distro: melodic
+          os_code_name: bionic
+
+  ros1_trigger:
+    name: ROS 1 Trigger
+    runs-on: ubuntu-18.04
+    strategy:
+      matrix:
+        python: ['2.7', '3.4', '3.5', '3.6']
+    steps:
+      - name: Check out project
+        uses: actions/checkout@v2
+      - name: Install dependencies
+        uses: ./.github/actions/setup
+      - name: Run job
+        uses: ./.github/actions/trigger
+        with:
+          ros_distro: melodic
+
   ros2_audit:
     name: ROS 2 Audit
     runs-on: ubuntu-20.04
@@ -301,3 +333,50 @@ jobs:
           os_code_name: 8
           arch: x86_64
           pkg_name: rcutils
+
+  ros2_sync_criteria_check:
+    name: ROS 2 Sync Criteria Check
+    runs-on: ubuntu-20.04
+    steps:
+      - name: Check out project
+        uses: actions/checkout@v2
+      - name: Install dependencies
+        uses: ./.github/actions/setup
+      - name: Run job
+        uses: ./.github/actions/sync_criteria_check
+        with:
+          config_url: https://raw.githubusercontent.com/ros2/ros_buildfarm_config/ros2/index.yaml
+          ros_distro: rolling
+          os_code_name: focal
+
+  ros2_sync_criteria_check_rpm:
+    name: ROS 2 Sync Criteria Check (RPM)
+    runs-on: ubuntu-20.04
+    steps:
+      - name: Check out project
+        uses: actions/checkout@v2
+      - name: Install dependencies
+        uses: ./.github/actions/setup
+      - name: Run job
+        uses: ./.github/actions/sync_criteria_check
+        with:
+          config_url: https://raw.githubusercontent.com/ros2/ros_buildfarm_config/ros2/index.yaml
+          config_name: rhel
+          ros_distro: rolling
+          os_name: rhel
+          os_code_name: 8
+          arch: x86_64
+
+  ros2_trigger:
+    name: ROS 2 Trigger
+    runs-on: ubuntu-20.04
+    steps:
+      - name: Check out project
+        uses: actions/checkout@v2
+      - name: Install dependencies
+        uses: ./.github/actions/setup
+      - name: Run job
+        uses: ./.github/actions/trigger
+        with:
+          config_url: https://raw.githubusercontent.com/ros2/ros_buildfarm_config/ros2/index.yaml
+          ros_distro: rolling

scripts/release/run_check_sync_criteria_job.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import print_function
+
 import argparse
 import copy
 import sys