ros-infrastructure/ros_buildfarm

commit 869449d838eb5fa96b7571949a7141168945f745

Add a script for culling Pulp content (#959)
This script walks the repository version history for a single repository
until it reaches the end, and begins removing repository versions from
oldest to newest.

After removing the repository versions, it triggers an orphan deletion
operation that actually deletes the content that was orphaned by the
removed repository versions.

ros_buildfarm/pulp.py
@@ -54,6 +54,9 @@ class PulpPageIterator:
         self._page = self._get_next(self._offset)
         self._offset += len(self._page.results)
         self._iter = iter(self._page.results)
+        logger.debug(
+            'Fetched a page of %d results (%d%%)' % (
+                len(self._page.results), 100.0 * self._offset / self._page.count))
 
     def __iter__(self):
         # Make sure we're at the beginning
@@ -95,6 +98,7 @@ class PulpRpmClient:
         # Core APIs
         self._core_client = pulpcore.ApiClient(self._config)
         self._core_tasks_api = pulpcore.TasksApi(self._core_client)
+        self._core_orphans_api = pulpcore.OrphansApi(self._core_client)
 
         # RPM APIs
         self._rpm_client = pulp_rpm.ApiClient(self._config)
@@ -103,6 +107,7 @@ class PulpRpmClient:
         self._rpm_publications_api = pulp_rpm.PublicationsRpmApi(self._rpm_client)
         self._rpm_remotes_api = pulp_rpm.RemotesRpmApi(self._rpm_client)
         self._rpm_repos_api = pulp_rpm.RepositoriesRpmApi(self._rpm_client)
+        self._rpm_repo_vers_api = pulp_rpm.RepositoriesRpmVersionsApi(self._rpm_client)
 
     def _wait_for_task(self, task_href):
         task = self._core_tasks_api.read(task_href)
@@ -163,6 +168,33 @@ class PulpRpmClient:
 
     def enumerate_remotes(self):
         return PulpPageIterator(self._rpm_remotes_api.list)
+
+    def enumerate_unused_repo_vers(self, distribution_name):
+        distribution = self._rpm_distributions_api.list(
+            name=distribution_name).results[0]
+        publication = self._rpm_publications_api.read(distribution.publication)
+        all_repo_vers = {
+            repo_ver.pulp_href: repo_ver for repo_ver in
+            PulpPageIterator(
+                self._rpm_repo_vers_api.list, rpm_rpm_repository_href=publication.repository,
+                limit=500)}
+
+        logger.debug(
+            'Fetched %d total repository versions for %s' % (
+                len(all_repo_vers), distribution_name))
+
+        unused_repo_vers = []
+        current_repo_ver = all_repo_vers.get(
+            all_repo_vers[publication.repository_version].base_version)
+        while current_repo_ver and current_repo_ver.number:
+            unused_repo_vers.append(current_repo_ver.pulp_href)
+            current_repo_ver = all_repo_vers.get(current_repo_ver.base_version)
+
+        logger.debug(
+            'Found %d ancestors of %s' % (
+                len(unused_repo_vers), publication.repository_version))
+
+        return unused_repo_vers
 
     def import_and_invalidate(
             self, distribution_name, packages_to_add,
@@ -304,6 +336,25 @@ class PulpRpmClient:
 
         if sync_task.created_resources:
             self._publish_and_distribute(distribution, sync_task.created_resources[0])
+
+    def remove_unused_content(self):
+        delete_task_href = self._core_orphans_api.delete().task
+        delete_task = self._wait_for_task(delete_task_href)
+        print('%s' % (delete_task,))
+
+    def remove_unused_repo_vers(self, distribution_name, dry_run=False):
+        unused_repo_vers = self.enumerate_unused_repo_vers(distribution_name)
+
+        # Start removing the oldest ones first
+        unused_repo_vers.reverse()
+
+        for repo_ver in unused_repo_vers:
+            if dry_run:
+                logger.debug('Removing %s (dry-run)' % (repo_ver,))
+                continue
+            logger.debug('Removing %s' % (repo_ver,))
+            delete_task_href = self._rpm_repo_vers_api.delete(repo_ver).task
+            self._wait_for_task(delete_task_href)
 
     def upload_pkg(self, file_path):
         relative_path = os.path.basename(file_path)

scripts/release/rpm/cull_repo.py
@@ -1,0 +1,60 @@
+#!/usr/bin/env python3
+
+# Copyright 2022 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import logging
+import sys
+
+from ros_buildfarm.argument import add_argument_dry_run
+from ros_buildfarm.argument import add_argument_pulp_base_url
+from ros_buildfarm.argument import add_argument_pulp_distribution_name
+from ros_buildfarm.argument import add_argument_pulp_password
+from ros_buildfarm.argument import add_argument_pulp_task_timeout
+from ros_buildfarm.argument import add_argument_pulp_username
+from ros_buildfarm.common import Scope
+from ros_buildfarm.pulp import PulpRpmClient
+
+
+def main(argv=sys.argv[1:]):
+    logging.basicConfig(
+        level=logging.DEBUG, format='%(name)s %(levelname)s %(asctime)s: %(message)s')
+
+    parser = argparse.ArgumentParser(
+        description='Cull unused repository snapshots and packages from Pulp')
+    add_argument_dry_run(parser)
+    add_argument_pulp_base_url(parser)
+    add_argument_pulp_distribution_name(parser)
+    add_argument_pulp_password(parser)
+    add_argument_pulp_task_timeout(parser)
+    add_argument_pulp_username(parser)
+    args = parser.parse_args(argv)
+
+    pulp_client = PulpRpmClient(
+        args.pulp_base_url, args.pulp_username, args.pulp_password,
+        task_timeout=args.pulp_task_timeout)
+
+    with Scope('SUBSECTION', 'removing unused snapshots'):
+        pulp_client.remove_unused_repo_vers(args.pulp_distribution_name, dry_run=args.dry_run)
+
+    with Scope('SUBSECTION', 'removing unused content'):
+        if args.dry_run:
+            print('Skipped (dry-run)')
+        else:
+            pulp_client.remove_unused_content()
+
+
+if __name__ == '__main__':
+    sys.exit(main())

commit effa6d48e5789db520d47bfd2790c8d3dce3be3f

Fix page percent division by zero
Fixes: 869449d838eb5fa96b7571949a7141168945f745

ros_buildfarm/pulp.py
@@ -54,9 +54,10 @@ class PulpPageIterator:
         self._page = self._get_next(self._offset)
         self._offset += len(self._page.results)
         self._iter = iter(self._page.results)
-        logger.debug(
-            'Fetched a page of %d results (%d%%)' % (
-                len(self._page.results), 100.0 * self._offset / self._page.count))
+        if self._page.count:
+            logger.debug(
+                'Fetched a page of %d results (%d%%)' % (
+                    len(self._page.results), 100.0 * self._offset / self._page.count))
 
     def __iter__(self):
         # Make sure we're at the beginning