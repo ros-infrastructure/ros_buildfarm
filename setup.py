f4091e64c0fe09dbee1e523448817667f6a74845
Scott K Logan <logans@cottsay.net>
2022-06-09 09:49:50 -0700

    List 'ROS Infrastructure Team' as the package maintainer (#952)
    This package is currently maintained by members of the ROS
    infrastructure team at Open Robotics.
    
    Since we're no longer providing an email address for the maintainer
    group (and we want folks to use the issue tracker anyway) let's make
    sure that the source code and issue tracker are listed in the package
    metadata.
    
    Co-authored-by: Steven! Ragnarök <steven@nuclearsandwich.com>

--- setup.py
+++ setup.py
@@ -41,8 +41,13 @@ kwargs = {
         'pyflakes'],
     'author': 'Dirk Thomas',
     'author_email': 'dthomas@osrfoundation.org',
-    'maintainer': 'Dirk Thomas',
-    'maintainer_email': 'dthomas@osrfoundation.org',
+    'maintainer': 'ROS Infrastructure Team',
+    'project_urls': {
+        'Source code':
+        'https://github.com/ros-infrastructure/ros_buildfarm',
+        'Issue tracker':
+        'https://github.com/ros-infrastructure/ros_buildfarm/issues',
+    },
     'url': 'https://github.com/ros-infrastructure/ros_buildfarm',
     'keywords': ['ROS', 'buildfarm', 'catkin'],
     'classifiers': [
