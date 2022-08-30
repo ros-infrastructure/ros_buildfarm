ros-infrastructure/ros_buildfarm

commit 642dd451ec5c9c7c8813997134009a71d1b440cd

Re-add script files which invoke the new modules

scripts/misc/docker_pull_baseimage.py
@@ -1,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Copyright 2022 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from runpy import run_module
+
+
+if __name__ == '__main__':
+    run_module('ros_buildfarm.scripts.misc.docker_pull_baseimage', run_name='__main__')

scripts/subprocess_reaper.py
@@ -1,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Copyright 2022 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from runpy import run_module
+
+
+if __name__ == '__main__':
+    run_module('ros_buildfarm.scripts.subprocess_reaper', run_name='__main__')

scripts/wrapper/apt.py
@@ -1,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Copyright 2022 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from runpy import run_module
+
+
+if __name__ == '__main__':
+    run_module('ros_buildfarm.scripts.wrapper.apt', run_name='__main__')

scripts/wrapper/git.py
@@ -1,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Copyright 2022 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from runpy import run_module
+
+
+if __name__ == '__main__':
+    run_module('ros_buildfarm.scripts.wrapper.git', run_name='__main__')

commit d52c049e8e3170e0ab359419680dcd517333ca42

Fix some scripts which didn't previously require ros_buildfarm
It seems that these four scripts didn't previously require the
ros_buildfarm module to be importable to run. Rather than update all of
their calls throughout ros_buildfarm (there are numerous), take the same
approach as was used in the git and apt wrapper scripts previously to
inject the module into the import search path.

Fixes: 642dd451ec5c9c7c8813997134009a71d1b440cd

ros_buildfarm/scripts/wrapper/apt.py
@@ -1,26 +1,0 @@
-# Copyright 2016 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import os
-import sys
-
-try:
-    from ros_buildfarm.wrapper.apt import main
-except ImportError:
-    sys.path.insert(
-        0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-    from ros_buildfarm.wrapper.apt import main
-
-if __name__ == '__main__':
-    main()

ros_buildfarm/scripts/wrapper/git.py
@@ -1,26 +1,0 @@
-# Copyright 2016 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import os
-import sys
-
-try:
-    from ros_buildfarm.wrapper.git import main
-except ImportError:
-    sys.path.insert(
-        0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-    from ros_buildfarm.wrapper.git import main
-
-if __name__ == '__main__':
-    main()

ros_buildfarm/wrapper/apt.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 # Copyright 2014-2016 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -169,4 +167,4 @@ def call_apt(argv, known_error_strings):
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    main()

ros_buildfarm/wrapper/git.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 # Copyright 2016 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -76,4 +74,4 @@ def call_git(argv, known_error_strings):
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+    main()

scripts/misc/docker_pull_baseimage.py
@@ -14,7 +14,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 from runpy import run_module
+import sys
+
+try:
+    from ros_buildfarm import __version__
+except ImportError:
+    sys.path.insert(
+        0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
 
 if __name__ == '__main__':

scripts/subprocess_reaper.py
@@ -14,7 +14,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 from runpy import run_module
+import sys
+
+try:
+    from ros_buildfarm import __version__
+except ImportError:
+    sys.path.insert(
+        0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
 
 if __name__ == '__main__':

scripts/wrapper/apt.py
@@ -14,8 +14,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 from runpy import run_module
+import sys
+
+try:
+    from ros_buildfarm import __version__
+except ImportError:
+    sys.path.insert(
+        0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
 
 if __name__ == '__main__':
-    run_module('ros_buildfarm.scripts.wrapper.apt', run_name='__main__')
+    run_module('ros_buildfarm.wrapper.apt', run_name='__main__')

scripts/wrapper/git.py
@@ -14,8 +14,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 from runpy import run_module
+import sys
+
+try:
+    from ros_buildfarm import __version__
+except ImportError:
+    sys.path.insert(
+        0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
 
 if __name__ == '__main__':
-    run_module('ros_buildfarm.scripts.wrapper.git', run_name='__main__')
+    run_module('ros_buildfarm.wrapper.git', run_name='__main__')