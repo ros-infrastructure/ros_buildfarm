337d41dc92a1c8c6cd6bb64cbd3fbc8cea61ef00
Scott K Logan <logans@cottsay.net>
2025-03-05 16:51:05 -0600

    Deprecate 'options' argument to 'expand_template'
    The only known use of the 'options' argument is to force EmPy to treat
    bangpaths as comments. This change promotes that option to an explicit
    keyword argument of 'expand_template', which decouples the API from the
    EmPy API.
    
    Any current uses of 'options' will continue to work, and the new
    'ignore_bangpath' option will take precedence in a collision.

--- ros_buildfarm/scripts/doc/generate_doc_script.py
+++ ros_buildfarm/scripts/doc/generate_doc_script.py
@@ -16,7 +16,6 @@ import argparse
 import re
 import sys
 
-from em import BANGPATH_OPT
 from em import Hook
 from ros_buildfarm.argument import add_argument_arch
 from ros_buildfarm.argument import add_argument_build_name
@@ -127,7 +126,7 @@ def main(argv=sys.argv[1:]):
             'scms': hook.scms,
             'scripts': scripts,
             'doc_path': doc_path},
-        options={BANGPATH_OPT: False})
+        ignore_bangpath=True)
     value = re.sub(r'(^| )python3 ', r'\1' + sys.executable + ' ', value, flags=re.M)
     print(value)
 
